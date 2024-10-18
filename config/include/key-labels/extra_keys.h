40b47a061cb384b709caf4d04b2188721825ca15
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2024-10-19 00:02:37 +0530

    feat: define a centralized, extensible keymap and configuration

---
+++ config/include/key-labels/extra_keys.h
@@ -0,0 +1,45 @@
+// The default layout has 34 keys. Additional keys can be added by pre-setting any of
+// the macros defined in this file to one or more keys before sourcing this file.
+
+/* left of left half */
+#ifndef X_LT  // top row, left
+    #define X_LT
+#endif
+#ifndef X_LM  // middle row, left
+    #define X_LM
+#endif
+#ifndef X_LB  // bottom row, left
+    #define X_LB
+#endif
+#ifndef X_LH  // thumb row, left
+    #define X_LH
+#endif
+
+/* between left and right half */
+#ifndef X_MT  // top row, middle
+    #define X_MT
+#endif
+#ifndef X_MM  // middle row, middle
+    #define X_MM
+#endif
+#ifndef X_MB  // bottom row, middle
+    #define X_MB
+#endif
+#ifndef X_MH  // thumb row, middle
+    #define X_MH
+#endif
+
+/* right of right half */
+#ifndef X_RT  // top row, right
+    #define X_RT
+#endif
+#ifndef X_RM  // middle row, right
+    #define X_RM
+#endif
+#ifndef X_RB  // bottom row, right
+    #define X_RB
+#endif
+#ifndef X_RH  // thumb row, right
+    #define X_RH
+#endif
+
